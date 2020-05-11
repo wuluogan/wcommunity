f352df6af4374c848372cdecb4c0dbb9521296b0
WLG <wlg@Mac.local>
2020-05-11 22:48:59 +0800

    20200511

---
+++ src/main/java/com/wlg/WCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.wlg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wlg/WCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
